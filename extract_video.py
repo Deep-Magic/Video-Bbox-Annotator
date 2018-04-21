ba180ff87b78bc7476bf1e3d2e9da93f4cb01ca8
hanskrupakar <hanskrupakar1994@gmail.com>
2018-04-20 22:40:29 -0400

    typos

--- extract_video.py
+++ extract_video.py
@@ -8,20 +8,20 @@ if __name__=='__main__':
     ap.add_argument('-r', '--rotation', required=True, help="Rotation angle for frames")
     args = ap.parse_args()
     
-    for vid in glob.glob(args.video_dir+'
-    if not os.path.isdir(os.path.basename(args.video_file)):
-        os.mkdir(os.path.basename(args.video_file))
-    
-    vidcap = cv2.VideoCapture(args.video_file)
-    success,image = vidcap.read()
-    count = 0
-    if int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT)) == len(glob.glob(os.path.join(os.path.basename(args.video_file), '*'))):
-        print ("Using cached video frames!!")
-    else:
-        print ("Creating frame image files!!")
-        while success:
-            if not os.path.exists('%s/%d.jpg'%(os.path.basename(args.video_file), count)):
-                image = imutils.rotate_bound(image, int(args.rotation))
-                cv2.imwrite('%s/%d.jpg'%(os.path.basename(args.video_file), count), image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-            count = count + 1
-            success,image = vidcap.read()
+    for vid in glob.glob(os.path.join(args.video_dir, '*')):
+        if not os.path.isdir(os.path.basename(vid)):
+            os.mkdir(os.path.basename(vid))
+        
+        vidcap = cv2.VideoCapture(vid)
+        success,image = vidcap.read()
+        count = 0
+        if int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT)) == len(glob.glob(os.path.join(os.path.basename(vid), '*'))):
+            print ("Using cached video frames!!")
+        else:
+            print ("Creating frame image files!!")
+            while success:
+                if not os.path.exists('%s/%d.jpg'%(os.path.basename(vid), count)):
+                    image = imutils.rotate_bound(image, int(args.rotation))
+                    cv2.imwrite('%s/%d.jpg'%(os.path.basename(vid), count), image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+                count = count + 1
+                success,image = vidcap.read()
