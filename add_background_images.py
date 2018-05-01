5aed3914e9a4a881af72aa5bf24a383046c1da5f
hanskrupakar <hanskrupakar1994@gmail.com>
2018-05-01 02:00:56 +0000

    bg_imgs

---
+++ add_background_images.py
@@ -0,0 +1,17 @@
+import argparse
+import csv
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-c', '--csv_file', help="CSV file to add background images to", required=True)
+ap.add_argument('-b', '--bg_dir', help="Directory with the background images", required=True)
+args = ap.parse_args()
+
+if __name__=='__main__':
+	
+	with open(args.csv_file, 'a') as f:
+	
+		writer = csv.writer(f, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		bg_imgs = glob.glob(os.path.join(os.path.abspath(args.bg_dir), '*'))
+		
+		for img in bg_imgs:
+			writer.writerow([img,'','','','',''])
